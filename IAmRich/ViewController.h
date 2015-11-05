cb1f2d75d48b30dabcea666cba363843c4c85a33
tuanvu <tuanvu@tuanvus-macbook-pro.local>
2015-11-05 22:54:56 +0700

    Initial Commit

---
+++ IAmRich/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAmRich
+//
+//  Created by tuanvu on 11/5/15.
+//  Copyright © 2015 tuanvu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
